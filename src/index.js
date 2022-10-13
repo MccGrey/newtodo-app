2f5159e8f12c2c26869bf7d223a7800b8b7e36d8
MccGrey <chuxzzie01@yahoo.com>
2022-10-13 07:07:39 +0100

    add strictmode wrap to import components in index.js - this is to eneable checks and warnings

--- src/index.js
+++ src/index.js
@@ -5,4 +5,9 @@ import TodoContainer from "./components/TodoContainer";
 
 const element = <h1>Hello from Create React App</h1>;
 
-ReactDOM.render(<TodoContainer />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <TodoContainer />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
