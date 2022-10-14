292baf2aca7f8d18a3e15e9c4f42c2589e6e48eb
MccGrey <chuxzzie01@yahoo.com>
2022-10-14 05:24:49 +0100

    intall browserrouter dependency

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 //component file
 import TodoContainer from "./components/TodoContainer";
+import { BrowserRouter as Router } from "react-router-dom";
 
 //stylesheet
 import "./App.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <TodoContainer />
+    <Router>
+      <TodoContainer />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
